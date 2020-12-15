0562f49d1ad3debb0df76b0c40def47e3eb1e574
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-12-15 22:53:07 +0300

    Updates

---
+++ precache-manifest.f5ceb6fd6c30ff26a5fb8f3ae48bc412.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e028a11252624250bf52600cc25092",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b779c0add345cef1296f",
+    "url": "/portfolio/static/css/main.0ab095ff.chunk.css"
+  },
+  {
+    "revision": "392a7a878b1748bca65e",
+    "url": "/portfolio/static/js/2.052fc22c.chunk.js"
+  },
+  {
+    "revision": "e1b83f20a9bd74e0c924a759358459ff",
+    "url": "/portfolio/static/js/2.052fc22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b779c0add345cef1296f",
+    "url": "/portfolio/static/js/main.7a46e550.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "edad463c281d09bf294a1e7bc491feb1",
+    "url": "/portfolio/static/media/CV.edad463c.pdf"
+  },
+  {
+    "revision": "9ba36e8adf5ced3f2c77c143648f8d9e",
+    "url": "/portfolio/static/media/food2.9ba36e8a.jpg"
+  },
+  {
+    "revision": "fafbb619f27917cce2fbc3faa74a14aa",
+    "url": "/portfolio/static/media/startup.fafbb619.svg"
+  }
+]);
